c27cdc6e56fcaef005f4a9039697c6124f70b88c
Govind <agovindchavanb@gmail.com>
2023-05-17 22:34:48 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -48,7 +48,7 @@ const App = () => {
           ))
         }
       </div>}
-    </div>
+    </div >
   )
 }
 
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 
-let Selection = ({ keys, applyColor }) => {
-  function colorChange(e) {
-    e.target.style.backgroundColor = applyColor().background;
-  }
 
-  return (
-    <div>
-      <div className={keys} onClick={colorChange}></div>
-    </div>
-  );
-};
-
-export default Selection;
+let Selection=({keys,applyColor})=>{
+    console.log(keys,applyColor())
+    console.log(applyColor())
+    function colorChange(e){
+        console.log(e.target)
+        let v=e.target;
+        v.style.backgroundColor=`${applyColor().background}`
+    }
+    return (
+        <div>
+            <div className={keys} onClick={colorChange}></div>
+        </div>
+    )
+}
+export default Selection
